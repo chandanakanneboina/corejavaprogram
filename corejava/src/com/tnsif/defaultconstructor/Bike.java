0521f6c030129cbb09db00b825815b5d3b7cee72
chand <chand@LAPTOP-8CU5MM0V>
2024-05-12 16:36:04 +0530

    first project

---
+++ corejava/src/com/tnsif/defaultconstructor/Bike.java
@@ -0,0 +1,11 @@
+package com.tnsif.defaultconstructor;
+public class Bike {
+	
+	Bike(){
+		System.out.println("bike is created");
+	}
+	public static void main(String[] args) {
+		Bike b=new Bike();
+	}
+
+}
---
+++ corejava/src/com/tnsif/sacnnerprogram/Taxcal.java
@@ -0,0 +1,17 @@
+package com.tnsif.sacnnerprogram;
+
+public class Taxcal {
+public void caluculatetax(Person p) {
+		
+		if(p.getIncome()<=1600) {
+			p.setTax(0);
+		}
+		else if(p.getIncome()>1600 && p.getIncome()<=15000) {
+			p.setTax(5);
+		}
+		else {
+			p.setTax(10);
+		}
+}
+}
+
