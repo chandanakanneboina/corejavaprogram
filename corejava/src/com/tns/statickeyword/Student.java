651b685bbe5f54ac0e55dd4ae21163e3ed4b6801
chand <chand@LAPTOP-8CU5MM0V>
2024-05-12 23:14:43 +0530

    FIRST PROJECT

---
+++ corejava/src/com/tns/statickeyword/Student.java
@@ -0,0 +1,21 @@
+package com.tns.statickeyword;
+
+public class Student {
+	int rollno;
+	String name;
+
+	static String college="ITS";
+
+	static void  change() {
+		   college="tkr";
+		               // static method
+		
+	}
+	Student(int r,String n){
+		rollno=r;
+		name=n;
+	}
+	void display1() {
+		System.out.println(rollno+" "+name+" "+college);
+	}
+}
---
+++ corejava/src/com/tnsif/finalkeyword/Finalvariable.java
@@ -0,0 +1,15 @@
+package com.tnsif.finalkeyword;
+public class Finalvariable {
+	static int x=9;
+	
+	static int y=4;
+	
+//	void change() {
+//		x=10;
+//	}
+	static {
+		x=6;
+		y=5;
+	}
+
+}
