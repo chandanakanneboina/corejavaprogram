e795db6168ee70d4ed9b0951e77b7081584eb6f8
chand <chand@LAPTOP-8CU5MM0V>
2024-05-12 23:37:09 +0530

    first proj

--- corejava/src/com/tnsif/finalkeyword/Finalmethod.java
+++ corejava/src/com/tnsif/finalkeyword/Finalmethod.java
@@ -2,4 +2,13 @@ package com.tnsif.finalkeyword;
 
 public class Finalmethod {
 
+	final float c=4.5f;
+	
+	
+	final void show() {      // final method
+		System.out.println("welcome to java");
+	}
+	
+
+
 }
